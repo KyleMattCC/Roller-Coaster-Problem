4689c1ec2d04067a6c03c9e94e6923a8ac09a07d
Janz Aeinstein Villamayor <janzvillamayor@yahoo.com>
2017-07-24 14:30:09 +0800

    [JFVillamayor] Method stubs of  threads

---
+++ src/threads/Car.java
@@ -0,0 +1,16 @@
+package threads;
+
+public class Car extends Thread{
+	public void load(){
+		
+	}
+	
+	public void unload(){
+		
+	}
+	
+	//Need to fix this
+	public void run(){
+		
+	}
+}
---
+++ src/threads/Passenger.java
@@ -0,0 +1,16 @@
+package threads;
+
+public class Passenger extends Thread{
+	public void board(){
+		
+	}
+	
+	public void unboard(){
+		
+	}
+	
+	@Override
+	public void run(){
+		
+	}
+}
